4531a387c6c2752fcbc453d6d8bf1a9fe76b5faa
alberto serrano <albert.serrano.ja@gmail.com>
2017-11-25 04:23:22 -0800

    feat: Add multiple cats

--- js/script.js
+++ js/script.js
@@ -1,30 +1,68 @@
+var cats = [
+    {
+        count: 0,
+        name: "Bob",
+        img: "images/cat1.jpg",
+        names: ['AlexMan', 'Dunken', 'Apples','Nugget']
+    },
+    {
+        count: 0,
+        name: "Dylan",
+        img: "images/cat2.jpg",
+        names: ['Harp']
+    },
+    {
+        count: 0,
+        name: "Maria",
+        img: "images/cat3.jpg",
+        names: ['Harpy']
+    },
+    {
+        count: 0,
+        name: "Abraham",
+        img: "images/cat4.jpg",
+        names: ['Lincoln']
+    },
+    {
+        count: 0,
+        name: "ZebroWiskey",
+        img: "images/cat5.jpg",
+        names: ['ZW']
+    }
+];
+
 var catView = function(){
+    var self = this;
     
-    this.currentCat = ko.observable(new Cat());  
+    this.catList = ko.observableArray([]);
+    
+    cats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem));
+        
+    });
+    
+    this.currentCat = ko.observable(this.catList()[0]);  
 
     this.incrementCounter = function(){
-        this.count(this.count() + 1);
+        self.currentCat().count(self.currentCat().count() + 1);
+    };
+    
+    this.setCat = function(clickedCat){
+        self.currentCat(clickedCat);
     };
         
 };
 
-var Cat = function(){
+var Cat = function(data){
     
-    this.name = ko.observable("Bob");
-    this.img= ko.observable("images/cat1.jpg");
-    this.count = ko.observable(0);
-    this.names = [
-        { nickname: 'AlexMan'},
-        { nickname: 'Dunken'},
-        { nickname: 'Apples'},
-        { nickname: 'Nugget'},
-        { nickname: 'Charles C. At'}
-    ];
+    this.name = ko.observable(data.name);
+    this.img = ko.observable(data.img);
+    this.count = ko.observable(data.count);
+    this.names = ko.observable(data.names);
     
     this.levels = ko.computed(function(){
         var stages = ["Baby", "Infant","Kid","Teen","Adult","Senior"];
         var num = this.count()/10;
-        console.log(num);
         if(num > 10){
             return "Grand Master";
         }else if(num > 5){
